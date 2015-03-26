6d77bc093c3eee7129cf37e9b0a898d61f7a814a
JungYG <sublime718@hotmail.com>
2015-03-26 23:08:49 +0900

    ~linkedlist.c

--- Linkedlist/linkedlist.c
+++ Linkedlist/linkedlist.c
@@ -5,8 +5,20 @@ typedef struct Node {
     struct Node *link;
 } Node;
 
-void createList(char);
-Node* searchNode(Node* p);
-void insertNode(Node* p);
-void removeNode(Node* p);
+int searchNode(Node* p, char);
+void insertNode(char);
+void removeNode(char);
 void printNode();
+
+int searchNode(Node* p, char data) {
+    struct Node* head;
+    int index = 0;
+    head = (Node*)malloc(sizeof(Node*));
+    head = p;
+    while (head->data != data) {
+        head = head->link;
+        index++;
+        if (head->link == NULL) return -1;
+    }
+    return index;
+}
