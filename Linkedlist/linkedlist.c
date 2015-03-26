9d46e52d5983c87855b0ec7da107835cabcd194c
JungYG <sublime718@hotmail.com>
2015-03-26 22:32:07 +0900

    linkedlist.c~

--- Linkedlist/linkedlist.c
+++ Linkedlist/linkedlist.c
@@ -4,3 +4,9 @@ typedef struct Node {
     char data;
     struct Node *link;
 } Node;
+
+void createList(char);
+Node* searchNode(Node* p);
+void insertNode(Node* p);
+void removeNode(Node* p);
+void printNode();
